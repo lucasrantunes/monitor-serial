5bf2b0669f36c7b2fbcc623e4fb11b8d7d659047
lucasrantunes <lucasribeiroantunes2013@gmail.com>
2022-01-16 22:11:11 -0300

    Improving read_loop function

--- main.py
+++ main.py
@@ -13,19 +13,22 @@ def read_loop() -> None:
     global arduino
     if(type(arduino) == Arduino):
         if arduino.get_record() == True:
-            text = arduino.read()
-            view.insert_text(text)
+            new_data = arduino.read()
+            insert_new_data(new_data)
             view.window.after(10, read_loop)
-            if view.get_auto_save() == 1:
-                file = open(f"log/{file_name}.txt", 'a', encoding='utf-8')
-                file.write(f"{text}\n")
+
+def insert_new_data(new_data):
+    view.insert_text(new_data)
+    if view.get_auto_save() == 1:
+        file = open(f"log/{file_name}.txt", 'a', encoding='utf-8')
+        file.write(f"{new_data}\n")
 
 def start_record() -> None:
     global arduino, file_name
     arduino = Arduino(view.get_port(), view.get_baudrate())
     arduino.set_record()
-    view.window.after(100, read_loop)
     file_name = datetime.now().strftime("%d%m%Y_%H%M%S")
+    view.window.after(100, read_loop)
 
 def stop_record() -> None:
     global arduino
@@ -42,12 +45,13 @@ def plot_last_record() -> None:
 def plot_record() -> None:
     graph = Graph(view.get_file_name())
 
-if __name__ == "__main__":
-    view = View(start_record, stop_record, plot_last_record, plot_record)
-
+def set_directory():
     current_directory = os.getcwd()
     folder_directory = os.path.join(current_directory, r"log") 
     if not os.path.exists(folder_directory):
         os.makedirs(folder_directory)
 
+if __name__ == "__main__":
+    view = View(start_record, stop_record, plot_last_record, plot_record)
+    set_directory()
     view.set_main_loop()
